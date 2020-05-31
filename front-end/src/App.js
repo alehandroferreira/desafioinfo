9565c5ec965a3f752b7a54b1fe5a8ad3e7d598d6
Alehandro Rocha <alehandro.ferreira@gmail.com>
2020-05-31 03:49:44 -0300

    Back-End e Front-End Funcionando Perfeiramente

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,36 +1,187 @@
 import React, { useState, useEffect } from "react";
+import { FiTrash2 } from 'react-icons/fi'
 
 import api from "./services/api";
 
+import "./global.css";
+import "./Sidebar.css";
+import "./Main.css";
 import "./App.css";
 
 import Header from "./components/Header";
-import { response } from "express";
 
 export default function App() {
-  const [cars, setCars] = useState(["Uno", "Gol", "Palio"]);
+  const [cars, setCars] = useState([]);
+
+  const [marca, setMarca] = useState('')
+  const [modelo, setModelo] = useState('')
+  const [ano, setAno] = useState('')
+  const [chassi, setChassi] = useState('')
+  const [placa, setPlaca] = useState('')
+  const [renavam, setRenavam] = useState('')
 
   useEffect(() => {
-    api.get('/car').then(response =>{
+    async function loadCars() {
+      const response = await api.get('/car')
 
+      setCars(response.data)
+    }
+
+    loadCars()
+  }, [])
+
+  async function handleAddCar(e) {
+    e.preventDefault()
+    // setCars([...cars, `New car ${Date.now()}`]);
+
+    const response = await api.post('/car', {
+      placa,
+      chassi,
+      renavam,
+      modelo,
+      marca,
+      ano
     })
-  }, []);
 
-  function handleAddCar() {
-    setCars([...cars, `New car ${Date.now()}`]);
+    setMarca('')
+    setModelo('')
+    setPlaca('')
+    setAno('')
+    setChassi('')
+    setRenavam('')
+
+    const car = response.data
+
+    setCars([...cars, car])
+
+  }
+
+  async function handleDeleteCar(id) {
+    try {
+      await api.delete(`car/${id}`)
+
+      setCars(cars.filter(car => car.id !== id))
+    } catch (err) {
+      alert('Erro ao deletar caso, tente novamente.')
+    }
+  }
+
+  async function handleUpdateCar(id){
+    try {
+      await api.put(`car/${id}`)
+    }
   }
 
   return (
     <>
-      <Header title="Homepage" />
-      <ul>
-        {cars.map((car) => (
-          <li key={car}> {car} </li>
-        ))}{" "}
-      </ul>
-      <button type="button" onClick={handleAddCar}>
-        Adicionar veículo
-      </button>
+      <Header title="Info Sistemas" />
+      <div id="app">
+        <aside>
+          <strong>Cadastrar</strong>
+          <form onSubmit={handleAddCar}>
+            <div className="input-block">
+              <label htmlFor="veicle_marca">Marca</label>
+              <input
+                type="text"
+                name="veicle_marca"
+                id="veicle_marca"
+                value={marca}
+                required
+                onChange={e => setMarca(e.target.value)}
+              />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="veicle_nome">Modelo</label>
+              <input
+                type="text"
+                name="veicle_nome"
+                id="veicle_nome"
+                value={modelo}
+                required
+                onChange={e => setModelo(e.target.value)}
+              />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="veicle_ano">Ano</label>
+              <input
+                type="number"
+                name="veicle_ano"
+                id="veicle_ano"
+                min="1990"
+                value={ano}
+                required
+                onChange={e => setAno(e.target.value)}
+              />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="veicle_chassi">Chassi</label>
+              <input
+                type="text"
+                name="veicle_chassi"
+                id="veicle_chassi"
+                value={chassi}
+                required
+                onChange={e => setChassi(e.target.value)}
+              />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="veicle_placa">Placa</label>
+              <input
+                type="text"
+                name="veicle_placa"
+                id="veicle_placa"
+                value={placa}
+                required
+                onChange={e => setPlaca(e.target.value)}
+              />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="veicle_renavam">Renavam</label>
+              <input
+                type="text"
+                name="veicle_renavam"
+                id="veicle_renavam"
+                value={renavam}
+                required
+                onChange={e => setRenavam(e.target.value)}
+              />
+            </div>
+
+            <button type="submit">
+              Cadastrar
+            </button>
+          </form>
+
+        </aside>
+        <main>
+
+          <ul>
+            {cars.map(car => (
+              <li key={car.id} className="car-item">
+                <header>
+                  <div className="car-info">
+                    <strong><span>Modelo:  </span>{car.modelo}</strong>
+                    <strong><span>Marca:  </span>{car.marca}</strong>
+                    <strong><span>Ano:  </span>{car.ano}</strong>
+                    <strong><span>Chassi:  </span>{car.chassi}</strong>
+                    <strong><span>Placa:  </span>{car.placa}</strong>
+                    <strong><span>Renavam:  </span>{car.renavam}</strong>
+                  </div>
+                </header>
+                <button type="button" onClick={() => handleDeleteCar(car.id)} >
+                  <FiTrash2 size={20} />
+                </button>
+              </li>
+            ))}
+          </ul>
+
+        </main>
+      </div>
     </>
   );
 }
--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
 
+import logoImage from '../assets/lg-info.png'
+
 export default function Header({ title, children }) {
   return (
-    <header>
-      <h1>{title}</h1>
+    <header id="logoInfo">
+      <img src={logoImage} width={300}/>
     </header>
   );
 }
